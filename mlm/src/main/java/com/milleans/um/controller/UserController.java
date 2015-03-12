8e839a09de18c2e78145b7047c2d6912470153e4
LeHu <hu.le.ca@gmail.com>
2015-03-11 21:48:07 -0400

    mmm111

--- mlm/src/main/java/com/milleans/um/controller/UserController.java
+++ mlm/src/main/java/com/milleans/um/controller/UserController.java
@@ -1,11 +1,9 @@
 package com.milleans.um.controller;
 
-import org.springframework.stereotype.Controller;
-
 /**
  * Created by macbookpro on 2015-03-07.
  */
-@Controller
+//@Controller
 public class UserController {
 
 
