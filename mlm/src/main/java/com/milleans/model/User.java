c3817135c2ca4f1ac9a42169e9c4c95ac31e9296
LeHu <hu.le.ca@gmail.com>
2015-03-16 23:11:16 -0400

    test eclipse

--- mlm/src/main/java/com/milleans/model/User.java
+++ mlm/src/main/java/com/milleans/model/User.java
@@ -11,249 +11,258 @@ import java.util.Date;
 @Table(name = "T_user")
 public class User {
 
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
 
-    @Column(name = "date")
-    private Date date;
+	@Column(name = "date")
+	private Date date;
 
-    @Column(name = "accountid")
-    private int accountId;
+	@Column(name = "accountid")
+	private int accountId;
 
-    @Column(name = "firstname")
-    private String firstName;
+	@Column(name = "firstname")
+	private String firstName;
 
-    @Column(name = "lastname")
-    private String lastName;
+	@Column(name = "lastname")
+	private String lastName;
 
-    @Column(name = "companyname")
-    private String companyName;
+	@Column(name = "companyname")
+	private String companyName;
 
-    @Column(name = "companytype")
-    private String companyType;
+	@Column(name = "companytype")
+	private String companyType;
 
-    @Column(name = "gender")
-    private String gender;
+	@Column(name = "gender")
+	private String gender;
 
-    @Column(name = "sponsorid")
-    private int sponsorid;
+	@Column(name = "sponsorid")
+	private int sponsorid;
 
-    @Column(name = "password")
-    private String passWord;
+	@Column(name = "password")
+	private String passWord;
 
-    @Column(name = "mobile")
-    private String mobile;
+	@Column(name = "mobile")
+	private String mobile;
 
-    @Column(name = "phone")
-    private String phone;
+	@Column(name = "phone")
+	private String phone;
 
-    @Column(name = "fax")
-    private String fax;
+	@Column(name = "fax")
+	private String fax;
 
-    @Column(name = "birthday")
-    private int birthDate;
-    @Column(name = "email")
-    private String email;
-    @Column(name = "address")
-    private String address;
-    @Column(name = "countryid")
-    private int countryId;
-    @Column(name = "province")
-    private String province;
-    @Column(name = "city")
-    private String city;
-    @Column(name = "zip")
-    private String zip;
-    @Column(name = "roleid")
-    private int roleId;
-    @Column(name = "ssn")
-    private String ssn;
-    @Column(name = "status")
-    private int status;
+	@Column(name = "birthday")
+	private int birthDate;
 
-    public Date getDate() {
-        return date;
-    }
+	@Column(name = "email")
+	private String email;
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+	@Column(name = "address")
+	private String address;
 
-    public int getAccountId() {
-        return accountId;
-    }
+	@Column(name = "countryid")
+	private int countryId;
 
-    public void setAccountId(int accountId) {
-        this.accountId = accountId;
-    }
+	@Column(name = "province")
+	private String province;
 
-    public String getFirstName() {
-        return firstName;
-    }
+	@Column(name = "city")
+	private String city;
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	@Column(name = "zip")
+	private String zip;
 
-    public String getLastName() {
-        return lastName;
-    }
+	@Column(name = "roleid")
+	private int roleId;
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	@Column(name = "ssn")
+	private String ssn;
 
-    public String getCompanyName() {
-        return companyName;
-    }
+	@Column(name = "status")
+	private int status;
 
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
+	public Date getDate() {
+		return date;
+	}
 
-    public String getCompanyType() {
-        return companyType;
-    }
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
-    public void setCompanyType(String companyType) {
-        this.companyType = companyType;
-    }
+	public int getAccountId() {
+		return accountId;
+	}
 
-    public String getGender() {
-        return gender;
-    }
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public int getSponsorid() {
-        return sponsorid;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public void setSponsorid(int sponsorid) {
-        this.sponsorid = sponsorid;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getPassWord() {
-        return passWord;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
-    }
+	public String getCompanyName() {
+		return companyName;
+	}
 
-    public String getMobile() {
-        return mobile;
-    }
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
 
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
+	public String getCompanyType() {
+		return companyType;
+	}
 
-    public String getPhone() {
-        return phone;
-    }
+	public void setCompanyType(String companyType) {
+		this.companyType = companyType;
+	}
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+	public String getGender() {
+		return gender;
+	}
 
-    public String getFax() {
-        return fax;
-    }
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
-    public void setFax(String fax) {
-        this.fax = fax;
-    }
+	public int getSponsorid() {
+		return sponsorid;
+	}
 
-    public int getBirthDate() {
-        return birthDate;
-    }
+	public void setSponsorid(int sponsorid) {
+		this.sponsorid = sponsorid;
+	}
 
-    public void setBirthDate(int birthDate) {
-        this.birthDate = birthDate;
-    }
+	public String getPassWord() {
+		return passWord;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getMobile() {
+		return mobile;
+	}
 
-    public String getAddress() {
-        return address;
-    }
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public String getPhone() {
+		return phone;
+	}
 
-    public int getCountryId() {
-        return countryId;
-    }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-    public void setCountryId(int countryId) {
-        this.countryId = countryId;
-    }
+	public String getFax() {
+		return fax;
+	}
 
-    public String getProvince() {
-        return province;
-    }
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
 
-    public void setProvince(String province) {
-        this.province = province;
-    }
+	public int getBirthDate() {
+		return birthDate;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public void setBirthDate(int birthDate) {
+		this.birthDate = birthDate;
+	}
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getZip() {
-        return zip;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
+	public String getAddress() {
+		return address;
+	}
 
-    public int getRoleId() {
-        return roleId;
-    }
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-    public void setRoleId(int roleId) {
-        this.roleId = roleId;
-    }
+	public int getCountryId() {
+		return countryId;
+	}
 
-    public String getSsn() {
-        return ssn;
-    }
+	public void setCountryId(int countryId) {
+		this.countryId = countryId;
+	}
 
-    public void setSsn(String ssn) {
-        this.ssn = ssn;
-    }
+	public String getProvince() {
+		return province;
+	}
 
-    public int getStatus() {
-        return status;
-    }
+	public void setProvince(String province) {
+		this.province = province;
+	}
 
-    public void setStatus(int status) {
-        this.status = status;
-    }
+	public String getCity() {
+		return city;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 }
