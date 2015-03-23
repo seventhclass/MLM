503f9fb2f2ca2cce3e601446034eca3826878b37
LeHu <hu.le.ca@gmail.com>
2015-03-22 23:10:06 -0400

    updated login json format

---
+++ mlm/src/main/java/com/milleans/LoginDto.java
@@ -0,0 +1,27 @@
+package com.milleans;
+
+/**
+ * Created by macbookpro on 2015-03-22.
+ */
+public class LoginDto {
+
+    private String result;
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
--- mlm/src/main/java/com/milleans/um/controller/UserController.java
+++ mlm/src/main/java/com/milleans/um/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.milleans.um.controller;
 
+import com.milleans.LoginDto;
 import com.milleans.model.User;
 import com.milleans.um.services.IUserService;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by macbookpro on 2015-03-07.
@@ -35,34 +37,43 @@ public class UserController {
             value = "/doLogin", method = RequestMethod.POST)
     public
     @ResponseBody
-    User
-    doLogin(String username, String password) {
+    LoginDto
+    doLogin(String memberId, String password, String autoFlag, HttpSession session) {
         log.info("user do login");
         System.out.println("dodododod");
-        User user = new User();
-        user.setFirstName("Hu");
-        user.setLastName("le");
-        user.setCity("Montreal");
-        user.setId(11);
-        user.setRoleId(77);
-        user.setAddress("5004 QueenMary");
-        user.setBirthDate(20001112);
-        user.setCompanyName("7th company");
-        user.setCompanyType("type");
-        user.setCountryId(86);
-        user.setDate(20150101);
-        user.setEmail("hu.le.ca@gmail.com");
-        user.setFax("5148850513");
-        user.setGender("M");
-        user.setMobile("4385570123");
-        user.setPassWord("111111");
-        user.setPhone("5148851234");
-        user.setProvince("QC");
-        user.setSponsorid(111);
-        user.setStatus(1);
-        user.setZip("h3w 1x2");
+//        User user = new User();
+//        user.setFirstName("Hu");
+//        user.setLastName("le");
+//        user.setCity("Montreal");
+//        user.setId(11);
+//        user.setRoleId(77);
+//        user.setAddress("5004 QueenMary");
+//        user.setBirthDate(20001112);
+//        user.setCompanyName("7th company");
+//        user.setCompanyType("type");
+//        user.setCountryId(86);
+//        user.setDate(20150101);
+//        user.setEmail("hu.le.ca@gmail.com");
+//        user.setFax("5148850513");
+//        user.setGender("M");
+//        user.setMobile("4385570123");
+//        user.setPassWord("111111");
+//        user.setPhone("5148851234");
+//        user.setProvince("QC");
+//        user.setSponsorid(111);
+//        user.setStatus(1);
+//        user.setZip("h3w 1x2");
+
+        session.setAttribute("userid", 11);
+        session.setAttribute("username", "Le Hu");
+
+        LoginDto loginDto = new LoginDto();
+        loginDto.setMessage("login success");
+        loginDto.setResult("success");
+
+
         log.info("user do login done");
-        return user;
+        return loginDto;
 
     }
 
