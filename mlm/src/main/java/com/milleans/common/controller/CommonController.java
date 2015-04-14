4b3d0822fbeed0e225ff056360620ff81ac4d44c
Vanness <le.hu@outlook.com>
2015-04-14 12:04:54 -0400

    fixed bug delete currency by le

--- mlm/src/main/java/com/milleans/common/controller/CommonController.java
+++ mlm/src/main/java/com/milleans/common/controller/CommonController.java
@@ -22,123 +22,119 @@ import java.util.List;
 @RequestMapping("/common")
 public class CommonController {
 
-    @Autowired
-    private ICurrencyService currencyService;
+	@Autowired
+	private ICurrencyService currencyService;
 
-    @Autowired
-    private ICategoryService categoryService;
+	@Autowired
+	private ICategoryService categoryService;
 
-    @RequestMapping(value = "/currency", method = RequestMethod.POST)
-    @ResponseBody
-    public CurrencyJs getCurrencyList() {
-        List<Currency> rs=currencyService.getAllCurrency();
-        CurrencyJs currencyJs=new CurrencyJs();
-        try {
-            currencyJs.setSymbol(rs);
-            currencyJs.setMessage("success");
-            currencyJs.setResult("success");
-        } catch (Exception e) {
-            currencyJs.setMessage("fail");
-            currencyJs.setResult("fail");
-        }
-        return currencyJs;
-    }
+	@RequestMapping(value = "/currency", method = RequestMethod.POST)
+	@ResponseBody
+	public CurrencyJs getCurrencyList() {
+		List<Currency> rs = currencyService.getAllCurrency();
+		CurrencyJs currencyJs = new CurrencyJs();
+		try {
+			currencyJs.setSymbol(rs);
+			currencyJs.setMessage("success");
+			currencyJs.setResult("success");
+		} catch (Exception e) {
+			currencyJs.setMessage("fail");
+			currencyJs.setResult("fail");
+		}
+		return currencyJs;
+	}
 
-    @RequestMapping(value = "/editcurrency",method = RequestMethod.POST)
-    @ResponseBody
-    public  CurrencyJs editCurrency(WebRequest webRequest){
+	@RequestMapping(value = "/editcurrency", method = RequestMethod.POST)
+	@ResponseBody
+	public CurrencyJs editCurrency(WebRequest webRequest) {
 
-        String model = webRequest.getParameter("model");
-        String id = webRequest.getParameter("id");
-        String name = webRequest.getParameter("name");
-        String abbr= webRequest.getParameter("abbr");
+		String model = webRequest.getParameter("model");
+		String id = webRequest.getParameter("id");
+		String name = webRequest.getParameter("name");
+		String abbr = webRequest.getParameter("abbr");
 
-        Currency currency=new Currency();
+		Currency currency = new Currency();
 
-        CurrencyJs currencyJs=new CurrencyJs();
+		CurrencyJs currencyJs = new CurrencyJs();
 
-        try {
-            if (model.equals("add")) { // save
-                currency.setSymbol(abbr);
-                currency.setName(name);
-                currencyService.save(currency);
-            } else if (model.equals("del")) { //del
-                currency.setId(Integer.valueOf(id));
-                currencyService.remove(currency);
-            } else { // update
-                currency.setId(Integer.valueOf(id));
-                currency.setName(name);
-                currency.setSymbol(abbr);
-                currencyService.update(currency);
-            }
-        } catch (Exception e) {
-            currencyJs.setMessage(e.getMessage());
-            currencyJs.setResult("fail");
-            e.printStackTrace();
-        }
+		try {
+			if (model.equals("add")) { // save
+				currency.setSymbol(abbr);
+				currency.setName(name);
+				currencyService.save(currency);
+			} else if (model.equals("del")) { // del
+				currency.setId(Integer.valueOf(id));
+				currencyService.remove(currency);
+			} else { // update
+				currency.setId(Integer.valueOf(id));
+				currency.setName(name);
+				currency.setSymbol(abbr);
+				currencyService.update(currency);
+			}
+		} catch (Exception e) {
+			currencyJs.setMessage(e.getMessage());
+			currencyJs.setResult("fail");
+			e.printStackTrace();
+		}
+		// currencyService.update(currency);
+		return currencyJs;
+	}
 
-        currencyService.update(currency);
+	@RequestMapping(value = "/category", method = RequestMethod.POST)
+	@ResponseBody
+	public CategoryJs getCategory() {
+		List<Category> rl = categoryService.getAllCategory();
+		CategoryJs categoryJs = new CategoryJs();
 
-        return currencyJs;
-    }
+		categoryJs.setList(rl);
+		categoryJs.setMessage("success");
+		categoryJs.setResult("success");
+		return categoryJs;
+	}
 
-    @RequestMapping(value = "/category", method = RequestMethod.POST)
-    @ResponseBody
-    public CategoryJs getCategory() {
-        List<Category> rl = categoryService.getAllCategory();
-        CategoryJs categoryJs = new CategoryJs();
+	@RequestMapping(value = "/editcategory", method = RequestMethod.POST)
+	@ResponseBody
+	public CategoryJs updateCategory(WebRequest webRequest) {
 
-        categoryJs.setList(rl);
-        categoryJs.setMessage("success");
-        categoryJs.setResult("success");
-        return categoryJs;
-    }
+		String model = webRequest.getParameter("model");
+		String id = webRequest.getParameter("id");
+		String name = webRequest.getParameter("name");
 
-    @RequestMapping(value = "/editcategory", method = RequestMethod.POST)
-    @ResponseBody
-    public CategoryJs updateCategory(WebRequest webRequest) {
+		Category category = new Category();
 
-        String model = webRequest.getParameter("model");
-        String id = webRequest.getParameter("id");
-        String name = webRequest.getParameter("name");
+		CategoryJs categoryJs = new CategoryJs();
 
-        Category category = new Category();
+		try {
+			if (model.equals("add")) { // save
+				category.setName(name);
+				if (categoryService.save(category) == 0) {
+					categoryJs.setMessage("add success!");
+					categoryJs.setResult("success");
+				} else {
+					categoryJs.setMessage("add fail!");
+					categoryJs.setResult("fail");
+				}
+			} else if (model.equals("del")) { // del
+				category.setId(Integer.valueOf(id));
+				categoryService.remove(category);
 
-        CategoryJs categoryJs = new CategoryJs();
+				categoryJs.setMessage("remove success!");
+				categoryJs.setResult("success");
+			} else { // update
+				category.setId(Integer.valueOf(id));
+				category.setName(name);
+				categoryService.update(category);
 
+				categoryJs.setMessage("update success!");
+				categoryJs.setResult("success");
+			}
+		} catch (Exception e) {
+			categoryJs.setMessage(e.getMessage());
+			categoryJs.setResult("fail");
+			e.printStackTrace();
+		}
 
-        try {
-            if (model.equals("add")) { // save
-                category.setName(name);
-                if (categoryService.save(category) == 0) {
-                    categoryJs.setMessage("add success!");
-                    categoryJs.setResult("success");
-                } else {
-                    categoryJs.setMessage("add fail!");
-                    categoryJs.setResult("fail");
-                }
-            } else if (model.equals("del")) { //del
-                category.setId(Integer.valueOf(id));
-                categoryService.remove(category);
-
-                categoryJs.setMessage("remove success!");
-                categoryJs.setResult("success");
-            } else { // update
-                category.setId(Integer.valueOf(id));
-                category.setName(name);
-                categoryService.update(category);
-
-                categoryJs.setMessage("update success!");
-                categoryJs.setResult("success");
-            }
-        } catch (Exception e) {
-            categoryJs.setMessage(e.getMessage());
-            categoryJs.setResult("fail");
-            e.printStackTrace();
-        }
-
-        return categoryJs;
-    }
+		return categoryJs;
+	}
 
 }
-
--- mlm/src/main/java/com/milleans/dao/AbstractDao.java
+++ mlm/src/main/java/com/milleans/dao/AbstractDao.java
@@ -1,7 +1,9 @@
 package com.milleans.dao;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -10,12 +12,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 public abstract class AbstractDao {
 
 	@Autowired
-	protected SessionFactory sessionFactory;
+	private SessionFactory sessionFactory;
 
 	public Object save(Object object) {
 		this.getCurrentSession().save(object);
 		return object;
 	}
+
+	public void delete(Object object) {
+		this.getCurrentSession().delete(object);
+	}
+
+	public void update(Object object) {
+		this.getCurrentSession().update(object);
+	}
+
+	// public List<T> getItemById(String id){
+	// Criteria criteria=this.getCurrentSession().createCriteria(T);
+	//
+	// }
 
 	public Session getCurrentSession() {
 		return this.sessionFactory.getCurrentSession();
