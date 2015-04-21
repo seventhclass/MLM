0d64a973f2f306b8b4111b48c22b173cbe80114d
LeHu <hu.le.ca@gmail.com>
2015-04-20 22:54:38 -0400

    zzz

--- mlm/src/main/java/com/milleans/service/ShipController.java
+++ mlm/src/main/java/com/milleans/service/ShipController.java
@@ -1,4 +1,4 @@
-package com.milleans.shipping.controller;
+package com.milleans.service;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
