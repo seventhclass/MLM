520833e3cbaa568ebd7889217665acbbd88d635c
LeHu <hu.le.ca@gmail.com>
2015-04-20 22:45:30 -0400

    updated uploadfile name

---
+++ mlm/src/main/java/com/milleans/shopping/controller/ShoppingCart.java
@@ -0,0 +1,19 @@
+package com.milleans.shopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by LeHu on 2015-04-20 10:42 PM.
+ */
+@Controller
+public class ShoppingCart {
+
+
+    @RequestMapping(value = "/myshoppingcart", method = RequestMethod.GET)
+    public ModelAndView myShoppingCart() {
+        return new ModelAndView("um/myshoppingcart");
+    }
+}
