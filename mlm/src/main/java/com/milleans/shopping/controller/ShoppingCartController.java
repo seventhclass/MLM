75012e9fd575c0c03c2ebdf61907cd10a7d52c32
LeHu <hu.le.ca@gmail.com>
2015-05-02 22:59:43 -0400

    zzz

--- mlm/src/main/java/com/milleans/shopping/controller/ShoppingCartController.java
+++ mlm/src/main/java/com/milleans/shopping/controller/ShoppingCartController.java
@@ -82,12 +82,17 @@ public class ShoppingCartController {
                 int _uis = userService.getUser(userId).getId();
                 shoppingCart = new ShoppingCart();
                 shoppingCart.setUserid(_uis);
-            }
 
-            shoppingCart.setProductId(Integer.valueOf(productId));
-            shoppingCart.setDate(new Date());
-            shoppingCart.setQuantity(Integer.valueOf(quantity));
-            shoppingCart.setTransactionPrice(Float.valueOf(price));
+                shoppingCart.setProductId(Integer.valueOf(productId));
+                shoppingCart.setDate(new Date());
+                shoppingCart.setQuantity(Integer.valueOf(quantity));
+                shoppingCart.setTransactionPrice(Float.valueOf(price));
+            }else {
+
+
+
+
+            }
 
             shoppingCartService.saveOrUpdate(shoppingCart);
 
