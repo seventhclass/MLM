e91edd1e5180266517312917cc5141c5d7f0df98
LeHu <hu.le.ca@gmail.com>
2015-05-03 15:22:58 -0400

    zzz

--- mlm/src/main/java/com/milleans/product/dao/ProductDao.java
+++ mlm/src/main/java/com/milleans/product/dao/ProductDao.java
@@ -1,8 +1,6 @@
 package com.milleans.product.dao;
 
 import com.milleans.dao.AbstractDao;
-import com.milleans.model.Category;
-import com.milleans.model.Currency;
 import com.milleans.model.Product;
 import com.milleans.product.dto.ProductTable;
 import com.milleans.shopping.dto.CartContent;
@@ -21,11 +19,6 @@ import java.util.List;
 @Repository("productDao")
 @Transactional
 public class ProductDao extends AbstractDao implements IProductDao {
-    //    @Override
-//    public Product save(Product product) {
-//        Product pro = (Product) this.getCurrentSession().save(product);
-//        return pro;
-//    }
 
     @Override
     public List<Product> getAllList() {
@@ -37,40 +30,50 @@ public class ProductDao extends AbstractDao implements IProductDao {
     @Override
     public List<ProductTable> getProductList() {
 
-        String hql = "select p, ca, cu from Product as p, Category as ca, Currency as cu " +
-                "where p.categoryId=ca.id and p.currencyId=cu.id";
+//        String hql = "select p, ca, cu , a.imageName from  Category as ca, Currency as cu, Album as a ," +
+//                "Product as p left join Album as b on p.id=b.productId " +
+//                " where p.categoryId=ca.id and p.currencyId=cu.id " +
+//                " group by p.id";
+//
+//        List<ProductTable> lsTable = new ArrayList<>();
+//        org.hibernate.Query query = this.getCurrentSession().createQuery(hql);
+//
+//        List rl = query.list();
+//
+//        for (Object object : rl) {
+//            ProductTable productTable = new ProductTable();
+//            Object[] objects = (Object[]) object;
+//            Product product = (Product) objects[0];
+//            Category category = (Category) objects[1];
+//            Currency currency = (Currency) objects[2];
+//
+//            productTable.setCapsuleNumber(product.getCapsuleNumber());
+//            productTable.setCategoryName(category.getName());
+//            productTable.setCategoryId(category.getId());
+//            productTable.setCurrencyName(currency.getName());
+//            productTable.setCurrencySymbol(currency.getSymbol());
+//            productTable.setCurrrencyId(currency.getId());
+//            productTable.setDate(product.getDate());
+//            productTable.setDescription(product.getDescription());
+//            productTable.setId(product.getId());
+//            productTable.setItemCode(product.getItemCode());
+//            productTable.setName(product.getName());
+//            productTable.setRetailPrice(product.getRetailPrice());
+//            productTable.setVolume(product.getVolume());
+//            productTable.setVolume2(product.getVolume2());
+//
+//            lsTable.add(productTable);
+//        }
 
-        List<ProductTable> lsTable = new ArrayList<>();
-        org.hibernate.Query query = this.getCurrentSession().createQuery(hql);
+//        return lsTable;
 
-        List rl = query.list();
+        String sql="";
 
-        for (Object object : rl) {
-            ProductTable productTable = new ProductTable();
-            Object[] objects = (Object[]) object;
-            Product product = (Product) objects[0];
-            Category category = (Category) objects[1];
-            Currency currency = (Currency) objects[2];
+        List<ProductTable> listTable=new ArrayList<>();
 
-            productTable.setCapsuleNumber(product.getCapsuleNumber());
-            productTable.setCategoryName(category.getName());
-            productTable.setCategoryId(category.getId());
-            productTable.setCurrencyName(currency.getName());
-            productTable.setCurrencySymbol(currency.getSymbol());
-            productTable.setCurrrencyId(currency.getId());
-            productTable.setDate(product.getDate());
-            productTable.setDescription(product.getDescription());
-            productTable.setId(product.getId());
-            productTable.setItemCode(product.getItemCode());
-            productTable.setName(product.getName());
-            productTable.setRetailPrice(product.getRetailPrice());
-            productTable.setVolume(product.getVolume());
-            productTable.setVolume2(product.getVolume2());
 
-            lsTable.add(productTable);
-        }
+        return listTable;
 
-        return lsTable;
     }
 
     @Override
@@ -93,9 +96,9 @@ public class ProductDao extends AbstractDao implements IProductDao {
         String hql = "select p,c,a from  User as u, ShoppingCart as c,Album as a, Product as p left join Album b on p.id=b.productId " +
                 " where u.userId=" + userId + " and u.id=c.id and c.productId=p.id group by p.id";
 
-        String sql = "select p.id,a.imagename,p.name,c.quantity,c.transactionPrice,p.capsulenumber from  t_user as u, t_shoppingcart as c, t_album as a, t_product as p " +
+        String sql = "select p.id,al.imagename,p.name,c.quantity,c.transactionPrice,p.capsulenumber from  t_user as u, t_shoppingcart as c, t_product as p " +
                 " left join t_album al on p.id = al.productid " +
-                " where u.userId=" + userId + " and u.id=c.id and c.productid=p.id group by p.id";
+                " where u.userId=" + userId + " and u.id=c.userid and c.productid=p.id group by p.id";
 
         List<CartContent> list = new ArrayList<>();
 
