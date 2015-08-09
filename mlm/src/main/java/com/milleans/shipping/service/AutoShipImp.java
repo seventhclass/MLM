869607009125cc2fa3e9627fe269c602b6cd7eb2
LeHu <hu.le.ca@gmail.com>
2015-08-09 19:36:47 -0400

    autoship updated

--- mlm/src/main/java/com/milleans/shipping/service/AutoShipImp.java
+++ mlm/src/main/java/com/milleans/shipping/service/AutoShipImp.java
@@ -3,9 +3,13 @@ package com.milleans.shipping.service;
 import com.milleans.dao.AbstractDao;
 import com.milleans.model.AutoShip;
 import com.milleans.shipping.dao.IAutoShipDao;
+import com.milleans.shipping.dto.ShipInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by LeHu on 7/29/15 2:39 PM.
@@ -20,6 +24,12 @@ public class AutoShipImp extends AbstractDao implements IAutoShip {
     @Override
     public int save(AutoShip autoShip) {
         return autoShipDao.save(autoShip);
+    }
+
+    @Override
+    public ArrayList<ShipInfo> getShipList(Date date) {
+
+        return autoShipDao.getShipList(date);
     }
 
     @Override
--- mlm/src/main/java/com/milleans/shipping/service/IAutoShip.java
+++ mlm/src/main/java/com/milleans/shipping/service/IAutoShip.java
@@ -2,6 +2,10 @@ package com.milleans.shipping.service;
 
 import com.milleans.model.AutoShip;
 import com.milleans.service.ICrud;
+import com.milleans.shipping.dto.ShipInfo;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by LeHu on 7/29/15 2:37 PM.
@@ -9,6 +13,11 @@ import com.milleans.service.ICrud;
 public interface IAutoShip extends ICrud {
 
     int save(AutoShip autoShip);
+
+    ArrayList<ShipInfo> getShipList(Date date);
+
+    ArrayList<String> getAutoShipOrderIdLs(Date date);
+
 
 
 }
